6f068974d3621505f7105df7ee88e4615d5c41f9
SviatDobrodij <sviatdobrodij@gmail.com>
2022-08-27 00:19:23 +0300

    markers and info window added

---
+++ awesome-project/src/app/pages/main/interfaces/marker.interface.ts
@@ -0,0 +1,12 @@
+export interface IMarker {
+    position: {
+        lat: number,
+        lng: number
+    };
+    label: {
+        color: string,
+        text: string
+    };
+    title?: string;
+    options?: { animation: google.maps.Animation.BOUNCE };
+}
--- awesome-project/src/app/pages/main/main.component.ts
+++ awesome-project/src/app/pages/main/main.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import * as mockData from '../../../assets/locations.json';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ILocationData } from 'src/app/shared/interfaces/locations.interface';
+import { IMarker } from './interfaces/marker.interface';
+import { MapInfoWindow, MapAnchorPoint } from '@angular/google-maps';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  @ViewChild(MapInfoWindow, { static: false }) infoWindow!: MapInfoWindow;
+
+  public zoom: number = 6;
+  public dataSource: ILocationData[] = { ...mockData }.data;
+  public center: google.maps.LatLngLiteral = {
+    lat: this.dataSource[0].coordinates[0],
+    lng: this.dataSource[0].coordinates[1]
+  };
+  public markers: IMarker[] = [];
 
   constructor() {
     //
-   }
+  }
+
+  ngOnInit(): void {
+    this.markers = this.dataSource.map(item => {
+      const eeee: IMarker = {
+        position: {
+          lng: item.coordinates[1],
+          lat: item.coordinates[0]
+        },
+        label: {
+          color: 'red',
+          text: item.name,
+        }
+      }
+
+      return eeee;
+    });
+  }
+
+  public openInfo(marker: MapAnchorPoint): void {
+    this.infoWindow?.open(marker);
+  }
 }
